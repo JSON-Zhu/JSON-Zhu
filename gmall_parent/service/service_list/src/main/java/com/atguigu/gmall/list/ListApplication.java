32b17617063aacb16723f60d8b5d26050aa87541
XQ.Z <xxx@qq.com>
2022-02-22 23:17:14 +0800

    --2022/2/21 --zhuxianqiang --第七天

---
+++ gmall_parent/service/service_list/src/main/java/com/atguigu/gmall/list/ListApplication.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * listApplication 搜索微服务的启动类
+ *
+ * @author XQ.Zhu
+ * @version 1.0
+ * 2022/2/22 10:42
+ **/
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.atguigu.gmall")
+public class ListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ListApplication.class,args);
+    }
+}
